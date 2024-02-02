842b49ada7e1e2c1e67a45aa307a85c499ada5a9
symbian1symbian1off <122437386+symbian1symbian1off@users.noreply.github.com>
2024-02-03 02:18:00 +0330

    Create age.php

---
+++ age.php
@@ -0,0 +1,15 @@
+<?php
+echo "how old are you "
+$n= "10"
+switch $n {
+case 5:
+    $result = "you are too young !!!";
+    break ;
+case 10 :
+    $result = "good age too start programing ";
+    break;
+default :
+    $result = "you must chose 10 or 5  as your age ";
+    break;
+}
+    ?>
